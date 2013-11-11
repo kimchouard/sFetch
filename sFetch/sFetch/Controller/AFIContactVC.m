1a90a75160c527c6703a1ce49a6fc52a0fd2c3c6
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2013-11-11 19:00:27 +0100

    ContactVC

--- sFetch/sFetch/Controller/AFIContactVC.m
+++ sFetch/sFetch/Controller/AFIContactVC.m
@@ -9,7 +9,10 @@
 #import "AFIContactVC.h"
 #import "AFISearchNavigationController.h"
 
-@interface AFIContactVC ()
+@interface AFIContactVC () <UICollectionViewDataSource, UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIPageControl *pageControl;
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 
 @end
 
@@ -28,6 +31,48 @@
     
     [navVC setDisplayedSearchString:self.contact.lastName];
     navVC.lastContactViewed = self.contact;    
+}
+
+#pragma mark UICollectionViewDataSource
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 3;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *identifier = @"tempCell";
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:identifier forIndexPath:indexPath];
+    
+    switch (indexPath.row) {
+        case 0:
+            cell.backgroundColor = [UIColor redColor];
+            break;
+        case 1:
+            cell.backgroundColor = [UIColor greenColor];
+            break;
+        case 2:
+            cell.backgroundColor = [UIColor blueColor];
+            break;
+    }
+    
+    
+    return cell;
+}
+
+
+#pragma mark UIScrollViewDelegate
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
+{
+    NSIndexPath *visiblePath = [[self.collectionView indexPathsForVisibleItems] lastObject];
+    self.pageControl.currentPage = visiblePath.row;
 }
 
 @end
